4b302b2139ceb7c56c0de70e734fef9a5cf13b2b
jon <jfnielse@umich.edu>
2021-04-30 14:19:36 -0400

    On branch develop 	new file:   plotblock.m

---
+++ +pulsegeq/plotblock.m
@@ -0,0 +1,34 @@
+function plotblock(blk, gamma, raster)
+% 3T: gamma = 4257.6 Hz/G
+% raster: 4e-6; 
+
+rf = [];
+gx = [];
+gy = [];
+gz = [];
+
+if ~isempty(blk.gx)
+	gx = pulsegeq.sub_trap2shape(blk.gx, gamma, raster);
+end
+if ~isempty(blk.gy)
+	gy = pulsegeq.sub_trap2shape(blk.gy, gamma, raster);
+end
+if ~isempty(blk.gz)
+	gz = pulsegeq.sub_trap2shape(blk.gz, gamma, raster);
+end
+if ~isempty(blk.rf)
+   rf = downsample(blk.rf.signal/gamma, round(raster/1e-6));  % Gauss
+end
+	
+subplot(121); hold on; 
+plot(raster*1e3*(1:length(gx)), gx, 'r.'); 
+plot(raster*1e3*(1:length(gy)), gy, 'g.'); 
+plot(raster*1e3*(1:length(gz)), gz, 'b.'); 
+xlabel('ms');
+ylabel('G/cm');
+%legend('gx (G/cm)', 'gy', 'gz');
+if ~isempty(rf)
+	subplot(122); 
+	plot(raster*1e3*(1:length(rf)), rf); ylabel('rf'); xlabel('ms');
+end
+

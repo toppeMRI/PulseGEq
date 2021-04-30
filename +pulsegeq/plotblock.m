42a41e01ff6c96eaa95ec496c922af4ed3cf74ee
jon <jfnielse@umich.edu>
2021-04-30 14:23:37 -0400

    On branch develop 	modified:   plotblock.m

--- +pulsegeq/plotblock.m
+++ +pulsegeq/plotblock.m
@@ -26,9 +26,10 @@ plot(raster*1e3*(1:length(gy)), gy, 'g.');
 plot(raster*1e3*(1:length(gz)), gz, 'b.'); 
 xlabel('ms');
 ylabel('G/cm');
-%legend('gx (G/cm)', 'gy', 'gz');
+legend('gx (G/cm)', 'gy', 'gz');
 if ~isempty(rf)
 	subplot(122); 
 	plot(raster*1e3*(1:length(rf)), rf); ylabel('rf'); xlabel('ms');
+	legend('rf (G)');
 end
 

83fff8108bc491979608e074cee5ef066c76a6fa
Jon-Fredrik Nielsen <jfnielse@umich.edu>
2022-11-20 13:02:42 -0500

    On branch develop 	modified:   flash3d2seq.m 	modified:   writeflash3d.m

--- examples/Pisa2022_PulseqForDummies/writeflash3d.m
+++ examples/Pisa2022_PulseqForDummies/writeflash3d.m
@@ -1,4 +1,3 @@
-function write3dflash_4ge
 
 sysGE = toppe.systemspecs('maxSlew', 20, 'slewUnit', 'Gauss/cm/ms', ...
     'maxGrad', 5, 'gradUnit', 'Gauss/cm', ...
@@ -11,45 +10,6 @@ FOV = [20 20 10];   % FOV (cm)
 flip = 10;          % degrees
 DTE = [0];          % extend TE by this much (ms). If vector, acquisitions are interleaved
 
-sub_flash(sysGE, N, FOV, flip, DTE, ...
-    'rfSpoilSeed', 117, ...
-    'nCyclesSpoil', 2);
-
-system('mv b0.tar 3dflash.tar');
-
-rf = toppe.readmod('tipdown.mod');
-toppe.plotseq(1, 2, sysGE, ...
-    'rhomax', 1.1*max(abs(rf))); 
-
-return
-
-function sub_flash(sys, N, FOV, flip, DTE, varargin)
-% function sub_flash(sys, N, FOV, flip, DTE, varargin)
-%
-% Fully-sampled 3D RF-spoiled GRE sequence for B0 (and B1-) mapping.
-% 
-% Inputs:
-%  sys        struct   scanner hardware settings. See toppe.systemspecs().
-%  N          [1 3]    matrix size
-%  FOV        [1 3]    field of view (cm)
-%  flip       [1 1]    flip angle (degrees)
-%  DTE        [1 n]    increase (from minimum value ) in TE for each of n scans (ms)
-%
-% Input options with defaults:
-%  entryFile = 'toppeN.entry';
-%  scanFilePath = '/usr/g/research/pulseq/cal/b0/';  
-%  tbw = 8;                     % RF pulse time-bandwidth product
-%  rfDur = 2;                   % RF pulse duration (ms)
-%  ftype = 'min';               % 'min': minimum-phase SLR pulse; 'ls': linear phase
-%  slabThick = 0.8*FOV(3);      % excited slab thickness
-%  rfSpoilSeed = 117;              % RF spoiling phase increment factor (degrees)
-%  exMod        = 'tipdown.mod';
-%  readoutMod   = 'readout.mod';
-%  nCyclesSpoil = 2;               % number of cycles of phase across voxel (along x and z)
-%  fatsat       = false;           % struct containing fat sat settings:
-%  fatFreqSign = -1;               %
-
-% defaults
 arg.entryFile = 'toppeN.entry';
 arg.scanFilePath = '/usr/g/research/pulseq/cal/b0/';
 arg.tbw = 8;                     % RF pulse time-bandwidth product
@@ -62,9 +22,6 @@ arg.readoutMod    = 'readout.mod';
 arg.nCyclesSpoil = 2;   % number of cycles of phase across voxel (along x and z)
 arg.fatsat       = false;         % add fat saturation pulse?
 arg.fatFreqSign = -1;            % sign of fatsat pulse frequency offset
-
-% substitute with provided keyword arguments
-arg = toppe.utils.vararg_pair(arg, varargin);
 
 nScans = numel(DTE);
 
@@ -109,24 +66,32 @@ if arg.fatsat
     fatsat.tbw     = 2.0;        % time-bandwidth product
     fatsat.dur     = 4.5;        % pulse duration (ms)
 
-    b1 = toppe.utils.rf.makeslr(fatsat.flip, fatsat.slThick, fatsat.tbw, fatsat.dur, 1e-6, sys, ...
+    b1 = toppe.utils.rf.makeslr(fatsat.flip, fatsat.slThick, fatsat.tbw, fatsat.dur, 1e-6, sysGE, ...
         'type', 'ex', ...    % fatsat pulse is a 90 so is of type 'ex', not 'st' (small-tip)
         'writeModFile', false);
     b1 = toppe.makeGElength(b1);
-    toppe.writemod(sys, 'rf', b1, 'ofname', 'fatsat.mod', 'desc', 'fat sat pulse');
+    toppe.writemod(sysGE, 'rf', b1, 'ofname', 'fatsat.mod', 'desc', 'fat sat pulse');
 end
 
-% excitation module
-[ex.rf, ex.g] = toppe.utils.rf.makeslr(flip, arg.slabThick, ...
-    arg.tbw, arg.rfDur, nz*arg.nCyclesSpoil, sys, ...
+% Excitation module
+% Add zeros at end (non-zero nChop) for conversion to Siemens
+toppe.utils.rf.makeslr(flip, arg.slabThick, ...
+    arg.tbw, arg.rfDur, nz*arg.nCyclesSpoil, sysGE, ...
     'ftype', arg.ftype, ...
     'spoilDerate', 0.5, ...
-    'ofname', arg.exMod);
+    'ofname', 'tmp.mod');
+
+[rf,gx,gy,gz,desc,paramsint16,paramsfloat,hdr] = toppe.readmod('tmp.mod');
+rf = [rf; zeros(48,1)];
+toppe.writemod(sysGE, 'rf', rf, 'gz', gz, 'desc', desc, ...
+    'nomflip', flip, ...  % needed to get right B1 scaling in .seq file
+    'nChop', [48 48], 'ofname', 'tipdown.mod');
+
 
 % readout module
 % Here we use the helper function 'makegre' to do that, but that's not a requirement.
 % Reduce slew to keep PNS in normal mode (<80% of limit)
-toppe.utils.makegre(FOV(1), N(1), voxSize(3), sys, ... 
+toppe.utils.makegre(FOV(1), N(1), voxSize(3), sysGE, ... 
     'ofname', arg.readoutMod, ...
     'ncycles', arg.nCyclesSpoil); 
 
@@ -137,7 +102,7 @@ rfSpoilSeed_cnt = 0;
 ny = N(2);
 nz = N(3);
 
-toppe.write2loop('setup', sys, 'version', 4);  % initialize file ('scanloop.txt')
+toppe.write2loop('setup', sysGE, 'version', 4);  % initialize file ('scanloop.txt')
 
 for iz = -1:nz     % We use iz<1 for approach to steady-state
     fprintf('\b\b\b\b\b\b\b\b\b\b\b\b%d of %d', max(1,iz), nz);
@@ -150,8 +115,8 @@ for iz = -1:nz     % We use iz<1 for approach to steady-state
 
             if(arg.fatsat)
                 fatChemShift = 3.5;  % fat/water chemical shift (ppm)
-                fatFreq = arg.fatFreqSign*sys.gamma*1e4*sys.B0*fatChemShift*1e-6;  % Hz
-                toppe.write2loop('fatsat.mod', sys, ...
+                fatFreq = arg.fatFreqSign*sysGE.gamma*1e4*sysGE.B0*fatChemShift*1e-6;  % Hz
+                toppe.write2loop('fatsat.mod', sysGE, ...
                     'RFoffset', round(fatFreq), ...   % Hz
                     'RFphase', rfphs);         % radians
 
@@ -159,12 +124,12 @@ for iz = -1:nz     % We use iz<1 for approach to steady-state
                 rfSpoilSeed_cnt = rfSpoilSeed_cnt + 1;
             end
 
-            toppe.write2loop(arg.exMod, sys, ...
+            toppe.write2loop(arg.exMod, sysGE, ...
                 'RFamplitude', 1.0, ...
                 'textra', DTE(ite), ...
                 'RFphase', rfphs);
 
-            toppe.write2loop(arg.readoutMod, sys, ...
+            toppe.write2loop(arg.readoutMod, sysGE, ...
                 'Gamplitude', [1.0 a_gy a_gz]', ...
                 'DAQphase', rfphs, ...
                 'textra', max(DTE) - DTE(ite), ... % to keep TR constant
@@ -176,26 +141,29 @@ for iz = -1:nz     % We use iz<1 for approach to steady-state
     end
 end
 fprintf('\n');
-toppe.write2loop('finish', sys);  % finalize file
+toppe.write2loop('finish', sysGE);  % finalize file
 
-fprintf('TR = %.3f ms\n', toppe.getTRtime(1, 2, sys)*1e3);
+fprintf('TR = %.3f ms\n', toppe.getTRtime(1, 2, sysGE)*1e3);
 
-%figure; toppe.plotseq(1, 4, sys);
+%figure; toppe.plotseq(1, 4, sysGE);
 
 % Create 'sequence stamp' file for TOPPE
 % This file is listed in line 6 of the .entry file
-toppe.preflightcheck(arg.entryFile, 'seqstamp.txt', sys);
+toppe.preflightcheck(arg.entryFile, 'seqstamp.txt', sysGE);
 
 % Write files to tar archive (for convenience).
-system(sprintf('tar cf b0.tar %s seqstamp.txt scanloop.txt modules.txt tipdown.mod readout.mod', arg.entryFile));
+system(sprintf('tar cf flash3d.tar %s seqstamp.txt scanloop.txt modules.txt tipdown.mod readout.mod', arg.entryFile));
 
 toppe.utils.scanmsg(arg.entryFile);
 
 % Play sequence in loop (movie) mode
 %nModulesPerTR = 2;
-%toppe.playseq(nModulesPerTR, sys, ...
+%toppe.playseq(nModulesPerTR, sysGE, ...
 %    'tpause', 0.05, ...
 %    'nTRskip', 8);
 
-return;
+return
+rf = toppe.readmod('tipdown.mod');
+toppe.plotseq(1, 2, sysGE, ...
+    'rhomax', 1.1*max(abs(rf))); 
 

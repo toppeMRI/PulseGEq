3f038e421757d770ca7c70209326bcc2600b05ca
Jon-Fredrik Nielsen <jfnielse@umich.edu>
2022-11-23 05:38:43 -0500

    On branch develop 	modified:   pns.m

--- examples/Pisa2022_PulseqForDummies/pns.m
+++ examples/Pisa2022_PulseqForDummies/pns.m
@@ -11,15 +11,17 @@ mxg = 5;
 % create a gradient trapezoid spoiler
 ncycles = 2;     % cycles of gradient spoiling across slthick
 slthick = 0.5;   % cm
-g = toppe.utils.makecrusher(ncycles,slthick,sysGE, 0, mxs,mxg);
+g1 = toppe.utils.makecrusher(ncycles,slthick,sysGE, 0, mxs,mxg);
 
-toppe.writemod(sysGE, 'gx', g, 'gz', g, 'ofname', 'tmp1.mod');
+g = g1;
+toppe.writemod(sysGE, 'gz', g, 'ofname', 'tmp1.mod');
 toppe.plotmod('tmp1.mod', 'gradient', 'xrm', 'printPNS', true);
 
-% make it a bipolar gradient
-g = [g; -g];
-toppe.writemod(sysGE, 'gx', g, 'gz', g, 'ofname', 'tmp2.mod');
-toppe.plotmod('tmp2.mod', 'gradient', 'xrm', 'printPNS', false);
+% make it a multipolar gradient
+g = [g1; -g1];
+g = [g; g; g];
+toppe.writemod(sysGE, 'gz', g, 'ofname', 'tmp3.mod');
+toppe.plotmod('tmp3.mod', 'gradient', 'xrm', 'printPNS', false);
 
 % create 'infinitely' long ramp to check stimulation threshold
 sysGE.maxSlew = 20;  % G/cm/ms
@@ -29,5 +31,5 @@ mxg = sysGE.maxGrad;
 dg = mxs*sysGE.raster*1e3;  % max gradient change per sample (per axis)
 g = 0:dg:mxg;
 g = [g fliplr(g) 0 0]';
-toppe.writemod(sysGE, 'gz', g, 'ofname', 'tmp3.mod');
-toppe.plotmod('tmp3.mod', 'gradient', 'xrm', 'printPNS', false);
+toppe.writemod(sysGE, 'gz', g, 'ofname', 'tmp4.mod');
+toppe.plotmod('tmp4.mod', 'gradient', 'xrm', 'printPNS', false);

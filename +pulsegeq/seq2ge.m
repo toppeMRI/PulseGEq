efe6e6f3a4587a710e88cc62dc981c7ecc970f94
Jon-Fredrik Nielsen <jfnielse@umich.edu>
2023-02-18 17:17:15 -0500

    On branch dev2 	modified:   seq2ge.m

--- +pulsegeq/seq2ge.m
+++ +pulsegeq/seq2ge.m
@@ -48,7 +48,7 @@ end
 % Get unique blocks
 uniqueBlocks{1} = seq.getBlock(1);
 
-for ib = 2:nt
+for ib = 1:nt
     if ~mod(ib, 500) | ib == nt
         for inb = 1:20
             fprintf('\b');
@@ -78,6 +78,7 @@ save uniqueBlocks uniqueBlocks
 fprintf('\n');
 return
 
+
 function issame = compareblocks(b1, b2)
 
     issame = true;
@@ -85,21 +86,92 @@ function issame = compareblocks(b1, b2)
     if b1.blockDuration ~= b2.blockDuration
         issame = false; return;
     end
-    if  xor(isempty(b1.rf), isempty(b2.rf)) | ...
+    if (xor(isempty(b1.rf), isempty(b2.rf)) | ...
         xor(isempty(b1.gx), isempty(b2.gx)) | ... 
         xor(isempty(b1.gy), isempty(b2.gy)) | ... 
         xor(isempty(b1.gz), isempty(b2.gz)) | ... 
-        xor(isempty(b1.adc), isempty(b2.adc))
+        xor(isempty(b1.adc), isempty(b2.adc)))
         issame = false; return;
     end
     if ~isempty(b1.rf)
-        if b1.rf.delay ~= b2.rf.delay
+        if ~comparerf(b1.rf, b2.rf)
             issame = false; return;
         end
     end
-        
+    if ~isempty(b1.gx)
+        if ~comparegradients(b1.gx, b2.gx)
+            issame = false; return;
+        end
+    end
+    if ~isempty(b1.gy)
+        if ~comparegradients(b1.gy, b2.gy)
+            issame = false; return;
+        end
+    end
+    if ~isempty(b1.gz)
+        if ~comparegradients(b1.gz, b2.gz)
+            issame = false; return;
+        end
+    end
+    if ~isempty(b1.adc)
+        if ~compareadc(b1.adc, b2.adc)
+            issame = false; return;
+        end
+    end
+return
+
+function issame = comparerf(rf1, rf2)
+
+    tol = 1e-4;  % defined to be equal if norm < tol
+
+    issame = true;
+
+    if (rf1.delay ~= rf1.delay | ...
+        rf1.shape_dur ~= rf2.shape_dur | ...
+        norm(rf1.t-rf2.t) > tol | ...
+        norm(rf1.signal-rf2.signal) > tol)
+        issame = false; return;
+    end
 
 return
+
+function issame = comparegradients(g1, g2)
+
+    tol = 1e-4;
+
+    issame = true;
+
+    if ~strcmp(g1.type, g2.type)
+        issame = false; return;
+    end
+    if strcmp(g1.type, 'trap')
+        if (g1.riseTime ~= g2.riseTime | ...
+            g1.flatTime ~= g2.flatTime | ...
+            g1.fallTime ~= g2.fallTime | ...
+            g1.delay ~= g2.delay)
+            issame = false; return;
+        end
+    else
+        % TODO: handle arbitrary waveforms
+    end
+
+return
+
+function issame = compareadc(adc1, adc2)
+
+    issame = true;
+
+    if (adc1.numSamples ~= adc2.numSamples | ...
+        adc1.dwell ~= adc2.dwell | ...
+        adc1.delay ~= adc2.delay)
+        issame = false;
+    end
+
+return
+
+
+
+
 
 if 1
     % get the next block, used to set textra column in scanloop.txt

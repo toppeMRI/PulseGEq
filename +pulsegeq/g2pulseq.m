a7cc4f763b27fc51e907e9c54635e516aa037f31
Jon-Fredrik Nielsen <jfnielse@umich.edu>
2023-06-25 19:06:36 -0400

    seq -> sys On branch dev/v6 	modified:   g2pulseq.m

--- +pulsegeq/g2pulseq.m
+++ +pulsegeq/g2pulseq.m
@@ -1,9 +1,9 @@
-function gout = g2pulseq(g,geRasterTime,seq)
-% convert gradient from Gauss/cm to Hz/m, and interpolate to seq.gradRasterTime
+function gout = g2pulseq(g,geRasterTime,sys)
+% convert gradient from Gauss/cm to Hz/m, and interpolate to sys.gradRasterTime
 gamma = 4.2576e3;         % Hz/G
 g = g * gamma * 100;   % Hz/m
 T = numel(g)*geRasterTime;    % pulse duration
 tge = 0:geRasterTime:(T-geRasterTime);
-t = 0:seq.gradRasterTime:(T-seq.gradRasterTime);
+t = 0:sys.gradRasterTime:(T-sys.gradRasterTime);
 gout = interp1(tge, g, t, 'linear', 'extrap');
 return;
